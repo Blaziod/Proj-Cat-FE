79977c74fdfedc1e8f13c96a164badb517f4becb
unknown <uwohchizoba@gmail.com>
2023-01-28 15:00:56 +0100

    stan second commit

--- src/LecturerRegister.jsx
+++ src/LecturerRegister.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 
-export const Register = (props) => {
+export const LecturerRegister = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
-    const [matric, setMatric] = useState('');
-    const [phone, setPhone] = useState('');
-    const [dept, setDept] = useState('');
-    const [sem, setSem] = useState('');
+  
 
 
     const handleSubmit = (e) => {
@@ -19,29 +16,15 @@ export const Register = (props) => {
     return (
 
         <div className="auth-form-container-register">
-            <h2>Student Register</h2>
+            <h2>Lecturer Register</h2>
             <form className="register-form" onSubmit={handleSubmit}>
                 <div className="form-grid-layout">
                     <div className="input-group">
                         <label htmlFor="name"> Full Name </label>
                         <input className="register-input" value={name} onChange={(e) => setName(e.target.value)} type="name" id="name" placeholder="full name" name="name" />
                     </div>
-                    <div className="input-group">
-                        <label htmlFor="matric"> Matric Number </label>
-                        <input className="register-input" value={matric} onChange={(e) => setMatric(e.target.value)} type="matric" id="matric" placeholder="matric number" matric="matric" />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="phone"> Phone Number </label>
-                        <input className="register-input" value={phone} onChange={(e) => setPhone(e.target.value)} type="phone" id="phone" placeholder="phone number" phone="phone" />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="dept"> Department </label>
-                        <input className="register-input" value={dept} onChange={(e) => setDept(e.target.value)} type="dept" id="dept" placeholder="Department" dept="dept" />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="sem"> Semester </label>
-                        <input className="register-input" value={sem} onChange={(e) => setSem(e.target.value)} type="sem" id="sem" placeholder="Semester" sem="sem" />
-                    </div>
+                
+                   
                     <div className="input-group">
                         <label htmlFor="email"> Email </label>
                         <input className="register-input" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
