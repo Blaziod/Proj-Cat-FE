5bc6757ea45eb1c98dda240a1e9cc2a48c790496
frankchoongsaeng <frankchoongsaeng@gmail.com>
2023-02-12 18:43:38 +0000

    page move

--- src/Pages/LecturerDashboard.jsx
+++ src/Pages/LecturerDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import data from "./mock-data.json";
+import data from "../mock-data.json";
 import "./Table.css";
 
 const LecturerDashboard = () => {
