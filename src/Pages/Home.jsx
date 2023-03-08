aa8fa1a9ca7f5279bf68cb8770d17410c652dc52
unknown <netstantechnology@gmail.com>
2023-03-08 20:41:59 +0100

    EDITED HOME

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,21 @@
-import { Link } from 'react-router-dom'
-import React from 'react'
-import { constants } from '../utils'
-import { useLoggedOut } from '../hooks/useProtected'
+import { Link } from "react-router-dom";
+import React from "react";
+import { constants } from "../utils";
+import { useLoggedOut } from "../hooks/useProtected";
 
 const Home = () => {
-	useLoggedOut()
-	return (
-		<div className='auth-form-container'>
-			<h1 className='app-container'>Home</h1>
-			<Link to={constants.routes.studentLogin}>
-				<button>STUDENT LOGIN</button>
-			</Link>{' '}
-			<br></br> <p></p>
-			<Link to={constants.routes.lecturerLogin}>
-				<button>LECTURER LOGIN</button>
-			</Link>
-		</div>
-	)
-}
+  useLoggedOut();
+  return (
+    <div>
+      <h1 className="app-container">Home</h1>
+      <Link to={constants.routes.studentLogin}>
+        <button>STUDENT LOGIN</button>
+      </Link>{" "}
+      <Link to={constants.routes.lecturerLogin}>
+        <button>LECTURER LOGIN</button>
+      </Link>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
